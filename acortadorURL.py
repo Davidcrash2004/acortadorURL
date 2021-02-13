8fcb0c2e7e7df6fd08df2408f1d26b22d6b117bd
Davidcrash2004 <77109489+Davidcrash2004@users.noreply.github.com>
2021-02-13 22:21:12 +0100

    Delete acortadorURL.py

--- acortadorURL.py
+++
@@ -1,9 +0,0 @@
-import pyshorteners
-
-def shorten(url):
-	link = pyshorteners.Shortener()
-	return link.tinyurl.short(url)
-
-if __name__ == "__main__":
-	url = input("Ingresa la URL que desees recortar: ")
-	print(f"\n{shorten(url)}")
